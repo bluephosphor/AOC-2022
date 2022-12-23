abc70d19698d29ab97421cb1faa42f26ee2eff44
Jonny Andino <66392799+bluephosphor@users.noreply.github.com>
2022-12-23 16:24:49 -0500

    1A Solution

--- solution/1A.ts
+++ solution/1A.ts
@@ -1,6 +1,20 @@
 import { getInput } from "..";
 
 const day = 1;
-const input = getInput(day);
+const input = getInput(day).split(/\r?\n/);
 
-console.log(input);
+let largestTotal = 0;
+let currentTotal = 0;
+
+for (const entry of input) {
+    if (entry !== '') {
+        // count calories!
+        currentTotal += parseInt(entry);
+    } else {
+        // new elf time
+        if (currentTotal > largestTotal) largestTotal = currentTotal;
+        currentTotal = 0;
+    }
+}
+
+console.log(largestTotal);
